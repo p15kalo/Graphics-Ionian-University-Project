e30f7468cd3eb72a1cd947be8a5c1713520c907a
p15kalo <agelos.agelinos.kaloheris6@gmail.com>
2023-01-18 19:01:32 +0200

    update

--- Assets/Script/destroyoncontact.cs
+++ Assets/Script/destroyoncontact.cs
@@ -6,7 +6,7 @@ public class destroyoncontact : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Respawn")
+        if (collision.gameObject.tag == "Sphere")
         {
             Destroy(collision.gameObject);
         }
